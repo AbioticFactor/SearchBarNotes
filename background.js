63a2b053aad957e67a8d7fdf47aa57a84c912d72
Abiotic <aoa34@cornell.edu>
2020-07-31 02:34:25 -0400

    commit bruh

--- background.js
+++ background.js
@@ -1,7 +1,36 @@
-var note;
-chrome.omnibox.onInputEntered.addListener(function (text){
-  note = text
-  console.log(note)
-  newElement()
+
+var note
+// Initialize array if not already stored
+try {
+  chrome.storage.sync.get(['notes'], function (data) {console.log(notes)});
+}
+catch (e) {
+  if (e instanceof ReferenceError) {
+    var noteArray = [];
+    chrome.storage.sync.set({ notes: noteArray }, function () {
+      console.log("added the note array");
+    });
+  } 
+}
+
+
+//On omnibox enter, add to array
+chrome.omnibox.onInputEntered.addListener(function (text) {
+  note = String(text)
+  console.log(String(note))
+
+
+  chrome.storage.sync.get(['notes'], function (data) {
+    console.log(data.notes);
+    update(data.notes);
+  });
 });
+
+function update(array) {
+  array.push(note);
+  //then call the set to update with modified value
+  chrome.storage.sync.set({notes: array }, function () {
+    console.log(array);
+  });
+}
 
--- popup.js
+++ popup.js
@@ -1,3 +1,46 @@
+// get sync data on load
+window.onload = function () {
+  chrome.storage.sync.get(['notes'], function (data) {
+    for (var i = 0; i < data.notes.length; i++) {
+      console.log("start");
+      newElement(data.notes[i])
+      console.log("done");
+    }
+  });
+}
+
+// Add a new element
+function newElement(addNote) {
+  var li = document.createElement("li");
+  var inputValue = addNote;
+  var t = document.createTextNode(inputValue);
+
+  console.log(inputValue)
+  console.log(t)
+
+  li.appendChild(t);
+
+  console.log(li)
+
+  if (document.getElementById("Tobi") == null) {
+    console.log("null")
+  }
+  document.getElementById("Tobi").appendChild(li);
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
 // Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
@@ -9,42 +52,52 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
-// Click on a close button to hide the current list item
+// Delete item from list
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
+    deleteItem;
+    alert("x")
     var div = this.parentElement;
     div.style.display = "none";
+
   }
 }
 
-//////////////////////////////////////////////
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = note;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
+function deleteItem(){
+  console.log("tesing it");
+  chrome.storage.sync.get(['notes'], function (data) {
+    data.notes.splice(0, 1);
+    var array = data.notes
+    chrome.storage.sync.set({notes: array}, function () {
+      console.log("deleted");
+    });
+  });
+}
+
+const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
+
+function switchTheme(e) {
+  if(e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
   }
-////////////////////////////////////////////////
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
+  else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
   }
 }
 
+toggleSwitch.addEventListener('change', switchTheme, false);
 
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
+}
 
